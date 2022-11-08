5c04be15f73651dd7fa905565b662ffdf5f99ba8
Dell <Dell@DESKTOP-5K7GC56>
2022-11-07 19:31:54 -0800

    Add project files.

---
+++ LeetcodeGrind/ArraysAndHashing/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeGrind.ArraysAndHashing;
+
+internal class Solution
+{
+    // 1. Two Sum
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            dict[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.TryGetValue(target - nums[i], out int result))
+            {
+                if (i != result)
+                {
+                    return new int[] { i, result };
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ LeetcodeGrindTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace LeetcodeGrindTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
