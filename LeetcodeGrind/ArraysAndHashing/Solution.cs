8a16261ba3e8c34796866b648527d08b6616c9ec
Nicholas Hebb <nhebb@breezetree.com>
2022-11-07 21:54:57 -0800

    Update Solution.cs
    First 3 prob

--- LeetcodeGrind/ArraysAndHashing/Solution.cs
+++ LeetcodeGrind/ArraysAndHashing/Solution.cs
@@ -8,6 +8,19 @@ namespace LeetcodeGrind.ArraysAndHashing;
 
 internal class Solution
 {
+    // 217. Contains Duplicate
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hs = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!hs.Add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+
+
     // 1. Two Sum
     public int[] TwoSum(int[] nums, int target)
     {
@@ -19,14 +32,37 @@ internal class Solution
 
         for (int i = 0; i < nums.Length; i++)
         {
-            if (dict.TryGetValue(target - nums[i], out int result))
+            if (dict.TryGetValue(target - nums[i], out int result)
+                && result != i)
             {
-                if (i != result)
-                {
-                    return new int[] { i, result };
-                }
+                return new int[] { i, result };
             }
         }
         return null;
+    }
+
+
+    // 242. Valid Anagram
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) { return false; }
+
+        var dict = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (!dict.TryAdd(c, 1))
+                dict[c]++;
+        }
+        foreach (char c in t)
+        {
+            if (!dict.ContainsKey(c))
+                return false;
+
+            dict[c] -= 1;
+            if (dict[c] == 0)
+                dict.Remove(c);
+        }
+        return true;
     }
 }
