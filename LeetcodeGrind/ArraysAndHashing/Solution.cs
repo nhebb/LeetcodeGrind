45cf23959958c47e18524fee6939b4089be4fede
Nicholas Hebb <nhebb@breezetree.com>
2022-11-08 00:08:02 -0800

    More solutions

--- LeetcodeGrind/ArraysAndHashing/Solution.cs
+++ LeetcodeGrind/ArraysAndHashing/Solution.cs
@@ -65,4 +65,128 @@ internal class Solution
         }
         return true;
     }
+
+
+    // 49. Group Anagrams
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var res = new List<IList<string>>();
+        var d = new Dictionary<string, List<string>>();
+        foreach (var s in strs)
+        {
+            var chars = string.Join("", s.ToCharArray().OrderBy(x => x));
+            if (!d.ContainsKey(chars))
+                d[chars] = new List<string>();
+            d[chars].Add(s);
+        }
+
+        foreach (var kvp in d)
+        {
+            res.Add(kvp.Value);
+        }
+        return res;
+    }
+
+
+    // 1299. Replace Elements with Greatest Element on Right Side
+    /* I did this one in Python3
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        lastMax = -1
+        for i in range(len(arr) -1, -1, -1):
+            curMax = max(lastMax, arr[i])
+            arr[i] = lastMax
+            lastMax = curMax
+            
+        return arr
+     */
+
+
+    // 392. Is Subsequence
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length > t.Length) { return false; }
+        if (s == string.Empty || s == t) { return true; }
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+            {
+                if (i == s.Length - 1) { return true; }
+                i++;
+            }
+            j++;
+        }
+        return false;
+    }
+
+
+    // 58. Length of Last Word
+    public int LengthOfLastWord(string s)
+    {
+        // The 'hard' way - without using Split()
+        var length = 0;
+        var instring = false;
+        var count = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                if (instring)
+                {
+                    instring = false;
+                    length = count;
+                    count = 0;
+                }
+                continue;
+            }
+            instring = true;
+            count++;
+        }
+        if (count > 0) length = count;
+
+        return length;
+    }
+
+
+    // 14. Longest Common Prefix
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs == null || strs.Length == 0)
+        {
+            return "";
+        }
+
+        var maxLength = int.MaxValue;
+        foreach (var str in strs)
+        {
+            if (str == null) { return ""; }
+            if (str.Length < maxLength) { maxLength = str.Length; }
+        }
+
+        if (maxLength == 0) { return ""; }
+
+        var longest = "";
+        int i = 1;
+        bool nomatch = false;
+        while (i <= maxLength)
+        {
+            var testPrefix = strs[0].Substring(0, i);
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (strs[j].Substring(0, i) != testPrefix)
+                {
+                    nomatch = true;
+                    break;
+                }
+            }
+            if (nomatch) { break; }
+            longest = testPrefix;
+            i++;
+        }
+        return longest;
+    }
 }
