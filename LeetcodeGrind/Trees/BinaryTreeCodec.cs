0ad23ecf6d1561f85b3de0eb7470a260ec0848d9
Nicholas Hebb <nhebb@breezetree.com>
2022-11-18 03:54:52 -0800

    Added BinaryTreeCodec

--- LeetcodeGrind/Trees/BinaryTreeCodec.cs
+++ LeetcodeGrind/Trees/BinaryTreeCodec.cs
@@ -17,12 +17,63 @@ public class BinaryTreeCodec
     // Encodes a tree to a single string.
     public string serialize(TreeNode root)
     {
+        var vals = new List<string>();
 
+        void Dfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                vals.Add("null");
+                return;
+            }
+            vals.Add(node.val.ToString());
+            Dfs(node.left);
+            Dfs(node.right);
+        }
+
+        Dfs(root);
+        return string.Join("#", vals);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(string data)
     {
+        /*  Input
+            [1,2,3,null,null,4,5]
+            Output
+            [1,2]
+            Expected
+            [1,2,3,null,null,4,5]
+         */
+        if (string.IsNullOrEmpty(data)) return null;
 
+        var vals = data.Split("#");
+        if (vals.Length == 0 || vals[0] == "null")
+            return null;
+
+        var root = new TreeNode(int.Parse(vals[0]));
+        var index = 1;
+
+        TreeNode Dfs()
+        {
+            if (index >= vals.Length) return null;
+            if (vals[index] == "null")
+            {
+                index++;
+                return null;
+            }
+
+            var node = new TreeNode(int.Parse(vals[index]));
+            index++;
+
+            node.left = Dfs();
+            node.right = Dfs();
+
+            return node;
+        }
+
+        root.left = Dfs();
+        root.right = Dfs();
+        return root;
     }
 }
