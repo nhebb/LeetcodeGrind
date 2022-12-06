88da96af55d51c0f0614a08d18f3506f2c16f689
Nicholas Hebb <nhebb@breezetree.com>
2022-12-05 19:40:59 -0800

    Added Find Missing Observations

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -25,4 +25,31 @@ public class MathAndGeometry
 
         return Math.Abs(slope1 - slope2) > double.Epsilon;
     }
+
+
+    // 2028. Find Missing Observations
+    public int[] MissingRolls(int[] rolls, int mean, int n)
+    {
+        // (rolls.Sum() + missing.Sum()) / (m + n) = mean
+        // missing.Sum() = mean * (m + n) - rolls.Sum()
+        var missingTotal = mean * (rolls.Length + n) - rolls.Sum();
+        var ans = new int[n];
+        Array.Fill(ans, 1);
+        missingTotal -= n;
+
+        while (missingTotal > 0)
+        {
+            for (int i = 0; i < n && missingTotal > 0; i++)
+            {
+                ans[i]++;
+                missingTotal--;
+            }
+        }
+
+        var impossible = ans.Any(x => x > 6) ||
+                         (rolls.Sum() + ans.Sum()) / ((double)(rolls.Length + n)) > mean;
+        if (impossible)
+            return Array.Empty<int>();
+        return ans;
+    }
 }
