6307a07e49110f9f8bdb5946dad6df45e4d1c4f2
Nicholas Hebb <nhebb@breezetree.com>
2022-12-07 02:56:03 -0800

    Added House Robber and House Robber II

---
+++ LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeGrind.DynamicProgramming;
+
+public class DynamicProgramming
+{
+    // 198. House Robber
+    public int Rob(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+        if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
+
+        var dp = new int[nums.Length];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.Length; i++)
+        {
+            dp[i] = Math.Max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[^1];
+    }
+
+
+    // 213. House Robber II
+    public int RobII(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+        if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
+
+        int RobDp(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+            if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
+
+            var dp = new int[nums.Length];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+
+            return dp[^1];
+        }
+
+        var arr1 = new ArraySegment<int>(nums, 0, nums.Length - 1).ToArray();
+        var arr2 = new ArraySegment<int>(nums, 1, nums.Length - 1).ToArray();
+
+        return Math.Max(RobDp(arr1), RobDp(arr2));
+    }
+}
