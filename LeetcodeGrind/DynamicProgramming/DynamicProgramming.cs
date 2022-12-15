46c72449832a613fc49f7805ee7e8d122365d599
Nicholas Hebb <nhebb@breezetree.com>
2022-12-15 13:47:46 -0800

    Added DP problems

--- LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
+++ LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
@@ -116,4 +116,46 @@ public class DynamicProgramming
 
         return maxLIS;
     }
+
+
+    // 1143. Longest Common Subsequence
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        var dp = new int[text1.Length + 1, text2.Length + 1];
+
+        for (int i = text1.Length - 1; i >= 0; i--)
+        {
+            for (int j = text2.Length - 1; j >= 0; j--)
+            {
+                if (text1[i] == text2[j])
+                    dp[i, j] = 1 + dp[i + 1, j + 1];
+                else
+                    dp[i, j] = Math.Max(dp[i + 1, j], dp[i, j + 1]);
+            }
+        }
+
+        return dp[0, 0];
+    }
+
+    // 62. Unique Paths
+    public int UniquePaths(int m, int n)
+    {
+        var dp = new int[m, n];
+
+        for (int i = 0; i < n; i++)
+            dp[m - 1, i] = 1;
+
+        for (int i = 0; i < m; i++)
+            dp[i, n - 1] = 1;
+
+        for (int i = m - 2; i >= 0; i--)
+        {
+            for (int j = n - 2; j >= 0; j--)
+            {
+                dp[i, j] = dp[i + 1, j] + dp[i, j + 1];
+            }
+        }
+
+        return dp[0, 0];
+    }
 }
