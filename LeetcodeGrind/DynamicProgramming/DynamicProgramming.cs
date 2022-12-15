1767fca88db595936c0c7a8cb2c94e4afacd30ca
Nicholas Hebb <nhebb@breezetree.com>
2022-12-15 00:13:15 -0800

    Added Longest Increasing Subsequence

--- LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
+++ LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -88,4 +89,31 @@ public class DynamicProgramming
         return dp[^1];
     }
 
+
+    // 300. Longest Increasing Subsequence
+    public int LengthOfLIS(int[] nums)
+    {
+        // Bottom up solution - iterates backwards through the array
+        // and sets the dp[i] value by checking every index of nums[]
+        // to the right of i. If the current nums[i] value is less than
+        // the checked nums[j] value, then the current LIS is set to
+        // 1 + dp[j] if it's longer the existing LIS.
+        var dp = new int[nums.Length];
+        dp[^1] = 1;
+        var maxLIS = 1;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            var curLIS = 1;
+            for (int j = i + 1; j < dp.Length; j++)
+            {
+                if (nums[i] < nums[j])
+                    curLIS = Math.Max(curLIS, 1 + dp[j]);
+            }
+            dp[i] = curLIS;
+            maxLIS = Math.Max(maxLIS, curLIS);
+        }
+
+        return maxLIS;
+    }
 }
