76bd59d744984f413f7dec9b0c62d4758bc68050
Nicholas Hebb <nhebb@breezetree.com>
2022-12-20 13:19:16 -0800

    Added MergeTrees

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -18,7 +18,7 @@ public class Trees
         {
             if (node == null) return;
 
-            DFS(node.left);
+            DFS(node.right);
             list.Add(node.val);
             DFS(node.right);
         }
@@ -415,5 +415,35 @@ public class Trees
         Dfs(root, 0);
 
         return sum;
+    }
+
+
+    // 617. Merge Two Binary Trees
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        if (root1 == null)
+            return root2;
+
+        if (root2 == null)
+            return root1;
+
+        void Dfs(TreeNode node1, TreeNode node2)
+        {
+            node1.val += node2.val;
+
+            if (node1.left == null && node2.left != null)
+                node1.left = node2.left;
+            else if (node2.left != null)
+                Dfs(node1.left, node2.left);
+
+            if (node1.right == null && node2.right != null)
+                node1.right = node2.right;
+            else if (node2.right != null)
+                Dfs(node1.right, node2.right);
+        }
+
+        Dfs(root1, root2);
+
+        return root1;
     }
 }
