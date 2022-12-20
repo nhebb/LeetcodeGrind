d2d26ae76cb95cba5d22e862adb2169bd103c495
Nicholas Hebb <nhebb@breezetree.com>
2022-12-19 19:09:26 -0800

    Added CanVisitAllRooms

--- LeetcodeGrind/Graphs/Graphs.cs
+++ LeetcodeGrind/Graphs/Graphs.cs
@@ -42,4 +42,28 @@ public class Graphs
 
         return false;
     }
+
+
+    // 841. Keys and Rooms
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var visited = new bool[rooms.Count];
+
+        var q = new Queue<int>();
+        q.Enqueue(0);
+
+        while (q.Count > 0)
+        {
+            var room = q.Dequeue();
+            visited[room] = true;
+            foreach (var key in rooms[room])
+            {
+                if (!visited[key])
+                    q.Enqueue(key);
+            }
+        }
+
+        var hasUnvisited = visited.Any(x => x == false);
+        return !hasUnvisited;
+    }
 }
