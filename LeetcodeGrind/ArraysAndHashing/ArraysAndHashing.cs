aa3dfabad56f888068016665c8ad0c58a9e02b9c
Nicholas Hebb <nhebb@breezetree.com>
2022-12-19 16:01:18 -0800

    Added MaxWidthOfVerticalArea and DailyTemperatures

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1142,4 +1142,19 @@ public class ArraysAndHashing
         Reverse(i + 1, nums.Length - 1);
     }
 
+
+    // 1637. Widest Vertical Area Between Two Points Containing No Points
+    public int MaxWidthOfVerticalArea(int[][] points)
+    {
+        var sortedPoints = points.OrderBy(x => x[0]).ToArray();
+
+        var maxDx = 0;
+
+        for (int i = 1; i < sortedPoints.Count(); i++)
+        {
+            maxDx = Math.Max(maxDx, sortedPoints[i][0] - sortedPoints[i - 1][0]);                
+        }
+
+        return maxDx;
+    }
 }
--- LeetcodeGrind/Stacks/Stacks.cs
+++ LeetcodeGrind/Stacks/Stacks.cs
@@ -8,5 +8,24 @@ namespace LeetcodeGrind.Stacks;
 
 public class Stacks
 {
+    // 739. Daily Temperatures
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var days = new int[temperatures.Length];
+        var stack = new Stack<int>();
 
+        for (int cur = 0; cur < temperatures.Length; cur++)
+        {
+            while (stack.Count > 0 && 
+                   temperatures[cur] > temperatures[stack.Peek()])
+            {
+                var prev = stack.Pop();
+                days[prev] = cur - prev;
+            }
+
+            stack.Push(cur);
+        }
+
+        return days;
+    }
 }
