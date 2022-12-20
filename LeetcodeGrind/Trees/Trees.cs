19da4b7dbac67ea99ddf984e55901067ccd3838f
Nicholas Hebb <nhebb@breezetree.com>
2022-12-20 04:51:29 -0800

    Added SumRootToLeaf

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -385,4 +385,35 @@ public class Trees
         }
         return result;
     }
+
+
+    // 1022. Sum of Root To Leaf Binary Numbers
+    public int SumRootToLeaf(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        var sum = 0;
+
+        void Dfs(TreeNode node, int val)
+        {
+            var curVal = node.val;
+            val = (val << 1) | curVal;
+
+            if (node.left == null && node.right == null)
+            {
+                sum += val;
+                return;
+            }
+
+            if (node.left != null)
+                Dfs(node.left, val);
+
+            if (node.right != null)
+                Dfs(node.right, val);
+        }
+
+        Dfs(root, 0);
+
+        return sum;
+    }
 }
