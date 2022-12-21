739a067fe14c677cc8e8948bfc5f95baf7fabb30
Nicholas Hebb <nhebb@breezetree.com>
2022-12-21 06:16:32 -0800

    Revised PartitionLabels

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1173,10 +1173,29 @@ public class ArraysAndHashing
         var idx1 = 0;
         while (idx1 < s.Length)
         {
-
+            var idx2 = d[s[idx1]];
+            if (idx1 == idx2)
+            {
+                ans.Add(1);
+                idx1++;
+            }
+            else
+            {
+                var maxIndex = idx2;
+                var j = idx1 + 1;
+                while (j < maxIndex)
+                {
+                    maxIndex = Math.Max(maxIndex, d[s[j]]);
+                    j++;
+                }
+                ans.Add(maxIndex - idx1 + 1);
+                idx1 = maxIndex + 1;
+            }
         }
-
 
         return ans;
     }
+
+
+    
 }
