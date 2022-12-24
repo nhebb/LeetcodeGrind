ffda225221a5abd38f59f59ffa3895961e7d6356
Nicholas Hebb <nhebb@breezetree.com>
2022-12-24 03:03:11 -0800

    Added CheckArithmeticSubarrays

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1237,6 +1237,31 @@ public class ArraysAndHashing
     }
 
 
+    // 1630. Arithmetic Subarrays
+    public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+    {
+        var ans = new List<bool>();
+
+        for (int i = 0; i < l.Length; i++)
+        {
+            var isArithmetic = true;
+            var segment = nums.Skip(l[i]).Take(r[i] - l[i] + 1).OrderBy(x => x).ToArray();
+            var delta = segment[1] - segment[0];
+            for (int j = 1; j < segment.Count() - 1; j++)
+            {
+                if (segment[j + 1] - segment[j] != delta)
+                {
+                    isArithmetic = false;
+                    break;
+                }
+            }
+            ans.Add(isArithmetic);
+        }
+
+        return ans;
+    }
+
+
     // 2500. Delete Greatest Value in Each Row
     public int DeleteGreatestValue(int[][] grid)
     {
