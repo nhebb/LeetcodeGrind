92e4e21c96207bb6a65e2d378bf0430ff189f798
Nicholas Hebb <nhebb@breezetree.com>
2022-12-24 04:23:20 -0800

    Improved GetAllElements performance

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -443,22 +443,53 @@ public class Trees
     // 1305. All Elements in Two Binary Search Trees
     public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
     {
-        var ans = new List<int>();
-
-        void Dfs(TreeNode node)
+        void Dfs(TreeNode node, List<int> list)
         {
             if (node.left != null)
-                Dfs(node.left);
-            ans.Add(node.val);
+                Dfs(node.left, list);
+            list.Add(node.val);
             if (node.right != null)
-                Dfs(node.right);
+                Dfs(node.right, list);
         }
 
-        if (root1 != null)
-            Dfs(root1);
-        if (root2 != null)
-            Dfs(root2);
+        var list1 = new List<int>();
+        var list2 = new List<int>();
 
-        return ans.OrderBy(x => x).ToList();
+        if (root1 != null)
+            Dfs(root1, list1);
+        if (root2 != null)
+            Dfs(root2, list2);
+
+        var ans = new List<int>(list1.Count + list2.Count);
+
+        int i = 0;
+        int j = 0;
+        while (i < list1.Count && j < list2.Count)
+        {
+            if (list1[i] < list2[j])
+            {
+                ans.Add(list1[i]);
+                i++;
+            }
+            else
+            {
+                ans.Add(list2[j]);
+                j++;
+            }
+        }
+
+        while (i < list1.Count)
+        {
+            ans.Add(list1[i]);
+            i++;
+        }
+
+        while (j < list2.Count)
+        {
+            ans.Add(list2[j]);
+            j++;
+        }
+
+        return ans;
     }
 }
