d36c1a93289bc8d3c7af44ac7cf9a6a468dbd2ae
Nicholas Hebb <nhebb@breezetree.com>
2022-12-25 05:41:45 -0800

    Added Intersection (of 2 arrays)

--- LeetcodeGrind/TwoPointers/TwoPointers.cs
+++ LeetcodeGrind/TwoPointers/TwoPointers.cs
@@ -272,4 +272,24 @@ public class TwoPointers
             k--;
         }
     }
+
+
+    // 349. Intersection of Two Arrays
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var hs1 = new HashSet<int>();
+        var hs2 = new HashSet<int>();
+
+        foreach (var num in nums1)
+            hs1.Add(num);
+        foreach (var num in nums2)
+            hs2.Add(num);
+
+        var ans = new List<int>();
+        foreach (var num in hs1)
+            if (hs2.Contains(num))
+                ans.Add(num);
+
+        return ans.ToArray();
+    }
 }
