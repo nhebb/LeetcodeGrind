e08bce932c4d9a0e0866e6ee918ec400a394008f
Nicholas Hebb <nhebb@breezetree.com>
2022-12-27 06:40:37 -0800

    Added OnesMinusZeros

--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -122,7 +123,7 @@ public class Matrices
                 var down = r < lastRow ? board[r + 1][c] : 0;
                 var downRight = r < lastRow && c < lastCol ? board[r + 1][c + 1] : 0;
 
-                var neighbors = upLeft + up + upRight 
+                var neighbors = upLeft + up + upRight
                               + left + right
                               + downLeft + down + downRight;
 
@@ -164,10 +165,50 @@ public class Matrices
                             max = grid[i][j];
                     }
                 }
-                ans[r-1][c-1] = max;
+                ans[r - 1][c - 1] = max;
             }
         }
 
         return ans;
+    }
+
+
+    // 2482. Difference Between Ones and Zeros in Row and Column
+    public int[][] OnesMinusZeros(int[][] grid)
+    {
+        var diff = new int[grid.Length][];
+        for (int r = 0; r < grid.Length; r++)
+        {
+            diff[r] = new int[grid[r].Length];
+        }
+
+        var rowVals = new int[grid.Length];
+        for (int i = 0; i < grid.Length; i++)
+        {
+            rowVals[i] = grid[i].Count(x => x == 1) -
+                         grid[i].Count(x => x == 0);
+        }
+
+        var colVals = new int[grid[0].Length];
+        for (int c = 0; c < grid[0].Length; c++)
+        {
+            for (int r = 0; r < grid.Length; r++)
+            {
+                if (grid[r][c] == 1)
+                    colVals[c]++;
+                else if (grid[r][c] == 0)
+                    colVals[c]--;
+            }
+        }
+
+        for (int r = 0; r < diff.Length; r++)
+        {
+            for (int c = 0; c < diff[r].Length; c++)
+            {
+                diff[r][c] = rowVals[r] + colVals[c];
+            }
+        }
+
+        return diff;
     }
 }
