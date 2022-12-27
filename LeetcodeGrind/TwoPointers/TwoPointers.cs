036ccbb701597c8fdd3457294e8d6bd2fd503037
Nicholas Hebb <nhebb@breezetree.com>
2022-12-26 18:59:40 -0800

    Added NumRescueBoats

--- LeetcodeGrind/TwoPointers/TwoPointers.cs
+++ LeetcodeGrind/TwoPointers/TwoPointers.cs
@@ -292,4 +292,28 @@ public class TwoPointers
 
         return ans.ToArray();
     }
+
+
+    // 881. Boats to Save People
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        Array.Sort(people);
+
+        var i = 0;
+        var j = people.Length - 1;
+        var count = 0;
+
+        while (i <= j)
+        {
+            count++;
+
+            if (i == j)
+                break;
+
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+        return count;
+    }
 }
