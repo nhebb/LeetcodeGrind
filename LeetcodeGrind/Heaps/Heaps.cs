89f2603f3137f76fb389f7e5b34ce1cf37e5417a
Nicholas Hebb <nhebb@breezetree.com>
2022-12-26 18:16:25 -0800

    Added MaximumBags

--- LeetcodeGrind/Heaps/Heaps.cs
+++ LeetcodeGrind/Heaps/Heaps.cs
@@ -145,6 +145,37 @@ public class Heaps
     }
 
 
+    // 2279. Maximum Bags With Full Capacity of Rocks
+    public int MaximumBags(int[] capacity, int[] rocks, int additionalRocks)
+    {
+        var pq = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < capacity.Length; i++)
+        {
+            var rocksNeeded = capacity[i] - rocks[i];
+            pq.Enqueue(rocksNeeded, rocksNeeded);
+        }
+
+        var count = 0;
+
+        while (pq.Count > 0 && additionalRocks > 0)
+        {
+            var numRocks = pq.Dequeue();
+            if (numRocks <= additionalRocks)
+            {
+                additionalRocks -= numRocks;
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+
     // 2418. Sort the People
     public string[] SortPeople(string[] names, int[] heights)
     {
