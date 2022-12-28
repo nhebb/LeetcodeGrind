d11f2da34adaadddc4d4abdb8a631df81188dc45
Nicholas Hebb <nhebb@breezetree.com>
2022-12-28 03:48:21 -0800

    Added CommonFactors

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -303,4 +303,32 @@ public class MathAndGeometry
             return Array.Empty<int>();
         return ans;
     }
+
+
+    // 2427. Number of Common Factors
+    public int CommonFactors(int a, int b)
+    {
+        var factors = new List<int>();
+        int lesser = a < b ? a : b;
+        var greater = a > b ? a : b;
+        var start = greater / 2;
+
+        for (int i = start; i > 1; i--)
+        {
+            if (greater % i == 0)
+                factors.Add(i);
+        }
+
+        var count = 1; // for "1" itself
+        foreach (var factor in factors)
+        {
+            if (factor <= lesser && lesser % factor == 0)
+                count++;
+        }
+
+        if (a == b && a > 1)
+            count++;
+
+        return count;
+    }
 }
