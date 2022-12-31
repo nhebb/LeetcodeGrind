01d226b3fa338860b4ac23af674e537b88f8363c
Nicholas Hebb <nhebb@breezetree.com>
2022-12-31 02:40:38 -0800

    Added MinMovesToSeat and CountGoodTriplets

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1312,6 +1312,32 @@ public class ArraysAndHashing
     }
 
 
+    // 1534. Count Good Triplets
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        var count = 0;
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            for (int j = i + 1; j < arr.Length - 1; j++)
+            {
+                for (int k = j + 1; k < arr.Length; k++)
+                {
+                    /* |arr[i] - arr[j]| <= a
+                     * |arr[j] - arr[k]| <= b
+                     * |arr[i] - arr[k]| <= c
+                     */
+
+                    if (Math.Abs(arr[i] - arr[j]) <= a &&
+                        Math.Abs(arr[j] - arr[k]) <= b &&
+                        Math.Abs(arr[i] - arr[k]) <= c)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
     // 1630. Arithmetic Subarrays
     public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
     {
@@ -1391,6 +1417,23 @@ public class ArraysAndHashing
 
         return count;
     }
+
+
+    // 2037. Minimum Number of Moves to Seat Everyone
+    public int MinMovesToSeat(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+
+        var moves = 0;
+        for (int i = 0; i < seats.Length; i++)
+        {
+            moves += Math.Abs(students[i] - seats[i]);
+        }
+
+        return moves;
+    }
+
 
 
     // 2221. Find Triangular Sum of an Array
