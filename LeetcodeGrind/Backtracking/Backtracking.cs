32fcf767abf0e1ac8af8b0410a18e959f7d1186e
Nicholas Hebb <nhebb@breezetree.com>
2022-12-30 22:21:23 -0800

    Added PermuteUnique

--- LeetcodeGrind/Backtracking/Backtracking.cs
+++ LeetcodeGrind/Backtracking/Backtracking.cs
@@ -124,6 +124,43 @@ public class Backtracking
     }
 
 
+    // 47. Permutations II
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        var permutations = new List<IList<int>>();
+        var hs = new HashSet<string>();
+        var permutation = new List<int>();
+        var chosen = new bool[nums.Length];
+
+        void Backtrack()
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (chosen[i])
+                    continue;
+
+                permutation.Add(nums[i]);
+                if (permutation.Count == nums.Length)
+                {
+                    var hash = string.Join(" ", permutation);
+                    if (hs.Add(hash))
+                        permutations.Add(new List<int>(permutation));
+                }
+                else
+                {
+                    chosen[i] = true;
+                    Backtrack();
+                    chosen[i] = false;
+                }
+                permutation.RemoveAt(permutation.Count - 1);
+            }
+        }
+
+        Backtrack();
+        return permutations;
+    }
+
+
     // 51. N-Queens
     public IList<IList<string>> SolveNQueens(int n)
     {
