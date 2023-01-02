12214bd318e9eca31e7b4af38b16a04482eb8467
Nicholas Hebb <nhebb@breezetree.com>
2023-01-02 05:33:16 -0800

    Added FindPeakElement

--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -4,12 +4,6 @@
     {
         static void Main(string[] args)
         {
-            var ah = new ArraysAndHashing.ArraysAndHashing();
-            var arr = new int[] { 4, 0, 3, 0 };
-            var res = ah.FindOriginalArray2(arr);
-
-
-            Console.WriteLine($"[{string.Join(",", res)}]");
 
         }
     }
