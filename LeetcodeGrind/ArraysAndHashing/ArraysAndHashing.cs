3fd98a489280a18b32b832235f9a4fb45ddef9c5
Nicholas Hebb <nhebb@breezetree.com>
2023-01-03 05:20:52 -0800

    Added WateringPlants and FindSmallestSetOfVertices

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1558,4 +1558,34 @@ public class ArraysAndHashing
         }
         return count;
     }
+
+
+    // 2079. Watering Plants
+    public int WateringPlants(int[] plants, int capacity)
+    {
+        var count = 0;
+        var water = capacity;
+
+        for (int i = 0; i < plants.Length; i++)
+        {
+            // walk to plant and water it
+            count += i + 1;
+            water -= plants[i];
+
+            // while water can has enough to fill neighnoring plant(s)
+            // increment count, i, and water the plant(s)
+            while (i < plants.Length - 1 && water >= plants[i + 1])
+            {
+                count++;
+                i++;
+                water -= plants[i];
+            }
+
+            // walk back to river and refill can
+            count += i + 1;
+            water = capacity;
+        }
+
+        return count;
+    }
 }
--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-
+            var edges = new List<IList<int>>();
+            edges.Add(new List<int>() { 0, 1 });
+            edges.Add(new List<int>() { 0, 2 });
+            edges.Add(new List<int>() { 2, 5 });
+            edges.Add(new List<int>() { 3, 4 });
+            edges.Add(new List<int>() { 4, 2 });
+            var n = 6;
+            var graphs = new Graphs.Graphs();
+            var ans = graphs.FindSmallestSetOfVertices(n, edges);
+            Console.WriteLine(string.Join(", ", ans));
         }
     }
 }
