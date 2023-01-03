b49b1464e2dfffdab6ae98810be3d8e146e9813a
Nicholas Hebb <nhebb@breezetree.com>
2023-01-02 16:56:27 -0800

    Added CountDigits

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -331,4 +331,25 @@ public class MathAndGeometry
 
         return count;
     }
+
+
+    // 2520. Count the Digits That Divide a Number
+    public int CountDigits(int num)
+    {
+        var val = num;
+        var count = 0;
+
+        while (val > 0)
+        {
+            var digit = val % 10;
+            val = val / 10;
+
+            if (digit == 0)
+                continue;
+
+            if (num % digit == 0)
+                count++;
+        }
+        return count;
+    }
 }
