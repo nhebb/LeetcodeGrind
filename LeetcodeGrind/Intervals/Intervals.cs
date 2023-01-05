7aa0e09622fe681f1d437e00ead740e27e8e3cf3
Nicholas Hebb <nhebb@breezetree.com>
2023-01-04 23:50:55 -0800

    Added FindMinArrowShots

--- LeetcodeGrind/Intervals/Intervals.cs
+++ LeetcodeGrind/Intervals/Intervals.cs
@@ -122,4 +122,23 @@ public class Intervals
         }
         return countsByHour.Values.Max();
     }
+
+
+    // 452. Minimum Number of Arrows to Burst Balloons
+    public int FindMinArrowShots(int[][] points)
+    {
+        Array.Sort(points, (x, y) => x[1].CompareTo(y[1]));
+        var lastEnd = points[0][1];
+        var count = 1;
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            if (points[i][0] <= lastEnd)
+                continue;
+            count++;
+            lastEnd = points[i][1];
+        }
+
+        return count;
+    }
 }
--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -4,10 +4,31 @@
     {
         static void Main(string[] args)
         {
-            var tasks = new int[] { 2, 2, 3, 3, 2, 4, 4, 4, 4, 4 };
-            var ah = new ArraysAndHashing.ArraysAndHashing();
-            var res = ah.MinimumRounds(tasks);
+            //[[],[],[],[],[],[],[]]
+            //var points = new int[7][];
+
+            //points[0] = new int[] { 9, 12 };
+            //points[1] = new int[] { 1, 10 };
+            //points[2] = new int[] { 4, 11 };
+            //points[3] = new int[] { 8, 12 };
+            //points[4] = new int[] { 3, 9 };
+            //points[5] = new int[] { 6, 9 };
+            //points[6] = new int[] { 6, 7 };
+
+            var points = new int[4][];
+            //points[0] = new int[] { 10, 16 };
+            //points[1] = new int[] { 2, 8 };
+            //points[2] = new int[] { 1, 6 };
+            //points[3] = new int[] { 7, 12 };
+
+            points[0] = new int[] { 1, 2 };
+            points[1] = new int[] { 3, 4 };
+            points[2] = new int[] { 5, 6 };
+            points[3] = new int[] { 7, 8 };
+
+            var intervals = new Intervals.Intervals();
+            var res = intervals.FindMinArrowShots(points);
             Console.WriteLine(res);
-        }   
+        }
     }
 }
