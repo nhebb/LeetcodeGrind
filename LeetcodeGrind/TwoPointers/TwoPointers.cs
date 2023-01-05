6173f8688ddd37a9036ddb7bbce21a6f737cfaab
Nicholas Hebb <nhebb@breezetree.com>
2023-01-05 03:51:41 -0800

    Added DiStringMatch

--- LeetcodeGrind/TwoPointers/TwoPointers.cs
+++ LeetcodeGrind/TwoPointers/TwoPointers.cs
@@ -316,4 +316,29 @@ public class TwoPointers
         }
         return count;
     }
+
+
+    // 942. DI String Match
+    public int[] DiStringMatch(string s)
+    {
+        var i = 0;
+        var j = s.Length;
+        var ans = new int[s.Length + 1];
+
+        for (var k = 0; k < s.Length; k++)
+        {
+            if (s[k] == 'I')
+            {
+                ans[k] = i;
+                i++;
+            }
+            else
+            {
+                ans[k] = j;
+                j--;
+            }
+        }
+        ans[^1] = i;
+        return ans;
+    }
 }
