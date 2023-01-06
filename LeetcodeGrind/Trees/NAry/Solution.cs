d2bb44185be492f96993f4773b1e29b6b26356c4
Nicholas Hebb <nhebb@breezetree.com>
2023-01-06 05:40:08 -0800

    Renamed folder and extracted NAryNode class

--- LeetcodeGrind/Trees/NAry/Solution.cs
+++ LeetcodeGrind/Trees/NAry/Solution.cs
@@ -4,30 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetcodeGrind.Trees.N_ary;
+namespace LeetcodeGrind.Trees.NAry;
 
 
-public class Solution
+public partial class Solution
 {
-    // Note: Changed class name from Node to NAryNode to
-    // avoid naming conflicts with other specialized node classes
-    public class NAryNode
-    {
-        public int val;
-        public IList<NAryNode> children;
-
-        public NAryNode() { }
-
-        public NAryNode(int val) => this.val = val;
-
-        public NAryNode(int val, IList<NAryNode> children)
-        {
-            this.val = val;
-            this.children = children;
-        }
-    }
-
-
     // 590. N-ary Tree Postorder Traversal
     public IList<int> Postorder(NAryNode root)
     {
