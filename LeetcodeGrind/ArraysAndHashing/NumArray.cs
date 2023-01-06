45a6ad35f87bb3da4bad8ce1fc575d979db41d0b
Nicholas Hebb <nhebb@breezetree.com>
2023-01-06 04:24:31 -0800

    Added NumArray

---
+++ LeetcodeGrind/ArraysAndHashing/NumArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeGrind.ArraysAndHashing;
+
+// 303. Range Sum Query - Immutable
+public class NumArray
+{
+    int[] arr;
+
+    public NumArray(int[] nums)
+    {
+        arr = nums;
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return arr[left..right].Sum();
+    }
+}
