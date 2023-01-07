57a6150ddd157f91f18f62e800212a28c36d74fe
Nicholas Hebb <nhebb@breezetree.com>
2023-01-07 06:41:52 -0800

    Added PivotInteger and NumOfStrings

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1862,4 +1862,39 @@ public class ArraysAndHashing
 
         return maxIndex;
     }
+
+
+    // 2485. Find the Pivot Integer
+    public int PivotInteger(int n)
+    {
+        var prefix = new int[n];
+        var postfix = new int[n];
+
+        prefix[0] = 1;
+        for (int i = 2; i < n; i++)
+            prefix[i - 1] = prefix[i - 2] + i;
+
+        postfix[^1] = n;
+        for (int i = n - 1; i > 0; i--)
+            postfix[i - 1] = postfix[i] + i;
+
+        for (int i = 0; i < n; i++)
+            if (prefix[i] == postfix[i])
+                return i + 1;
+
+        return -1;
+    }
+
+
+    // 1967. Number of Strings That Appear as Substrings in Word
+    public int NumOfStrings(string[] patterns, string word)
+    {
+        var count = 0;
+        foreach (var pattern in patterns)
+        {
+            if (word.Contains(pattern))
+                count++;
+        }
+        return count;
+    }
 }
--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -4,30 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //[[],[],[],[],[],[],[]]
-            //var points = new int[7][];
-
-            //points[0] = new int[] { 9, 12 };
-            //points[1] = new int[] { 1, 10 };
-            //points[2] = new int[] { 4, 11 };
-            //points[3] = new int[] { 8, 12 };
-            //points[4] = new int[] { 3, 9 };
-            //points[5] = new int[] { 6, 9 };
-            //points[6] = new int[] { 6, 7 };
-
-            var points = new int[4][];
-            //points[0] = new int[] { 10, 16 };
-            //points[1] = new int[] { 2, 8 };
-            //points[2] = new int[] { 1, 6 };
-            //points[3] = new int[] { 7, 12 };
-
-            points[0] = new int[] { 1, 2 };
-            points[1] = new int[] { 3, 4 };
-            points[2] = new int[] { 5, 6 };
-            points[3] = new int[] { 7, 8 };
-
-            var tp = new TwoPointers.TwoPointers();
-            var res = tp.SmallestNumber("IIIDIDDD");
+            var ah = new ArraysAndHashing.ArraysAndHashing();
+            var res = ah.PivotInteger(4);
             Console.WriteLine(res);
         }
     }
