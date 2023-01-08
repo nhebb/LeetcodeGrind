6495b5d328f64127c744912370a05212f271ebfb
Nicholas Hebb <nhebb@breezetree.com>
2023-01-08 03:17:36 -0800

    Added HammingDistance

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -398,4 +398,19 @@ public class MathAndGeometry
 
         return maxPoints;
     }
+
+
+    // 461. Hamming Distance
+    public int HammingDistance(int x, int y)
+    {
+        var val = x ^ y;
+        var hammingDistance = 0;
+        while (val > 0)
+        {
+            if ((val & 1) == 1)
+                hammingDistance++;
+            val >>= 1;
+        }
+        return hammingDistance;
+    }
 }
