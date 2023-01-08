c2e295a90e5aa727ec495829feeb269f66a7eff0
Nicholas Hebb <nhebb@breezetree.com>
2023-01-08 06:00:04 -0800

    Added ContainsNearbyAlmostDuplicate

--- LeetcodeGrind/SlidingWindow/SlidingWindow.cs
+++ LeetcodeGrind/SlidingWindow/SlidingWindow.cs
@@ -236,4 +236,42 @@ internal class SlidingWindow
 
         return res.ToArray();
     }
+
+
+    // 220. Contains Duplicate III
+    private class DiffNum
+    {
+        public int Index { get; private set; }
+        public int Value { get; private set; }
+        public DiffNum(int index, int value)
+        {
+            Index = index;
+            Value = value;
+        }
+    }
+    public bool ContainsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff)
+    {
+        var nums2 = new List<DiffNum>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums2.Add(new DiffNum(i, nums[i]));
+        }
+
+        var sorted = nums2.OrderBy(x => x.Value)
+                          .ThenBy(x => x.Index)
+                          .ToList();
+
+        for (int i = 0; i < sorted.Count - 1; i++)
+        {
+            var j = i + 1;
+            while (j < sorted.Count &&
+                   Math.Abs(sorted[j].Value - sorted[i].Value) <= valueDiff)
+            {
+                if (Math.Abs(sorted[j].Index - sorted[i].Index) <= indexDiff)
+                    return true;
+                j++;
+            }
+        }
+        return false;
+    }
 }
