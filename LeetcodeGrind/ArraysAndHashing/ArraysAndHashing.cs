2500f74568355e123b141b5cc9f2e45f5252345c
Nicholas Hebb <nhebb@breezetree.com>
2023-01-08 03:12:25 -0800

    Added GenerateMatrix and FindMaxConsecutiveOnes

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1897,4 +1897,16 @@ public class ArraysAndHashing
         }
         return count;
     }
+
+
+    // 485. Max Consecutive Ones
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        var s = string.Join("", nums);
+        var arr = s.Split('0', StringSplitOptions.RemoveEmptyEntries);
+        var max = 0;
+        foreach (var ones in arr)
+            max = Math.Max(max, ones.Length);
+        return max;
+    }
 }
--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -211,4 +211,75 @@ public class Matrices
 
         return diff;
     }
+
+
+    // 59. Spiral Matrix II
+    public int[][] GenerateMatrix(int n)
+    {
+        var matrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+        }
+
+        var count = 1;
+        var offset = 0;
+        var total = n * n;
+        var r = 0;
+        var c = 0;
+
+        while (count <= total)
+        {
+            // special case for last cell in odd-length matrix
+            if (count == total && n % 2 == 1)
+            {
+                matrix[n / 2][n / 2] = total;
+                break;
+            }
+
+            r = offset;
+            c = offset;
+
+            // top row
+            while (count <= total && c < n - 1 - offset)
+            {
+                matrix[r][c] = count;
+                count++;
+                c++;
+            }
+
+            // right column
+            while (count <= total && r < n - 1 - offset)
+            {
+                matrix[r][c] = count;
+                count++;
+                r++;
+            }
+
+            // bottom row
+            while (count <= total && c > offset)
+            {
+                matrix[r][c] = count;
+                count++;
+                c--;
+            }
+
+            // left column
+            while (count <= total && r > offset)
+            {
+                matrix[r][c] = count;
+                count++;
+                r--;
+            }
+
+            offset++;
+        }
+        //PrintMatrix(matrix);
+        return matrix;
+    }
+    private void PrintMatrix(int[][] matrix)
+    {
+        foreach (var row in matrix)
+            Console.WriteLine(string.Join("\t", row));
+    }
 }
