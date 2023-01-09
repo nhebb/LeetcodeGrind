b6f3b35474f31603ce7dd251488bc2fd175158a2
Nicholas Hebb <nhebb@breezetree.com>
2023-01-09 05:52:15 -0800

    Added CategorizeBox

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -413,4 +413,29 @@ public class MathAndGeometry
         }
         return hammingDistance;
     }
+
+
+    // 2525. Categorize Box According to Criteria
+    public string CategorizeBox(int length, int width, int height, int mass)
+    {
+        var heavy = mass >= 100;
+
+        var volume = ((long)width) * length * height;
+        var bulky = width >= 10000 || 
+                    height >= 10000 || 
+                    length >= 10000 || 
+                    volume >= 1_000_000_000;
+
+        string cat;
+        if (bulky && heavy)
+            cat = "Both";
+        else if (bulky)
+            cat = "Bulky";
+        else if (heavy)
+            cat = "Heavy";
+        else
+            cat = "Neither";
+
+        return cat;
+    }
 }
