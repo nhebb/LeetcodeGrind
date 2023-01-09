e8b9bfe9589a741091814f3cef12c2cb1455a927
Nicholas Hebb <nhebb@breezetree.com>
2023-01-09 04:32:42 -0800

    Added ValidMountainArray

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1909,4 +1909,35 @@ public class ArraysAndHashing
             max = Math.Max(max, ones.Length);
         return max;
     }
+
+
+    // 941. Valid Mountain Array
+    public bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3)
+            return false;
+
+        var increasing = true;
+        var i = 0;
+        var j = arr.Length - 1;
+        while (increasing && i < j)
+        {
+            increasing = false;
+            if (arr[i] < arr[i + 1])
+            {
+                i++;
+                increasing = true;
+            }
+
+            if (arr[j - 1] > arr[j])
+            {
+                j--;
+                increasing = true;
+            }
+
+            if (i == j && i > 0 && j < arr.Length - 1)
+                return true;
+        }
+        return false;
+    }
 }
