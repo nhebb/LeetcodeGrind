3a70b4a04f8e77e5eefeea3b446e114f6a41545f
Nicholas Hebb <nhebb@breezetree.com>
2023-01-10 01:57:34 -0800

    Added SumNumbers

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -622,4 +622,27 @@ public class Trees
 
         return CreateTree(0, nums.Count - 1);
     }
+
+
+    // 129. Sum Root to Leaf Numbers
+    public int SumNumbers(TreeNode root)
+    {
+        var sb = new StringBuilder();
+        var sum = 0;
+
+        void Dfs(TreeNode node)
+        {
+            sb.Append(node.val);
+            if (node.left == null && node.right == null)
+            {
+                sum += int.Parse(sb.ToString());
+            }
+            if (node.left != null) Dfs(node.left);
+            if (node.right != null) Dfs(node.right);
+
+            sb.Remove(sb.Length - 1, 1);
+        }
+        Dfs(root);
+        return sum;
+    }
 }
