f909af52adfecc6a4389879484b865079f4d7d1f
Nicholas Hebb <nhebb@breezetree.com>
2023-01-10 01:29:03 -0800

    Added ValidTree

--- LeetcodeGrind/Graphs/Graphs.cs
+++ LeetcodeGrind/Graphs/Graphs.cs
@@ -202,4 +202,39 @@ public class Graphs
 
         return true;
     }
+
+
+    // 261. Graph Valid Tree (locked)
+    public bool ValidTree(int n, int[][] edges)
+    {
+        if (n == 0) return true;
+
+        var adj = new List<List<int>>(n);
+        foreach (var edge in edges)
+        {
+            adj[edge[0]].Add(edge[1]);
+            adj[edge[1]].Add(edge[0]);
+        }
+
+        var visited = new HashSet<int>();
+        bool Dfs(int i, int prev)
+        {
+            if (!visited.Add(i))
+                return false;
+
+            var result = true;
+            foreach (var neighbor in adj[i])
+            {
+                if (neighbor != prev)
+                {
+                    if(! Dfs(neighbor, i))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        return Dfs(0, -1) && visited.Count == n;
+        
+    }
 }
