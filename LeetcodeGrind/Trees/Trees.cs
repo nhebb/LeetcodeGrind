b399b10fee5f458b20312846a976b27d741c2732
Nicholas Hebb <nhebb@breezetree.com>
2023-01-10 02:58:39 -0800

    Added FindBottomLeftValue

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -645,4 +645,34 @@ public class Trees
         Dfs(root);
         return sum;
     }
+
+
+    // 513. Find Bottom Left Tree Value
+    public int FindBottomLeftValue(TreeNode root)
+    {
+        var maxDepth = 0;
+        var minCol = 0;
+        var result = root.val;
+
+        void Dfs(TreeNode node, int depth, int col)
+        {
+            if (depth > maxDepth
+                || (depth == maxDepth && col < minCol))
+            {
+                maxDepth = depth;
+                minCol = col;
+                result = node.val;
+            }
+
+            if (node.left != null)
+                Dfs(node.left, depth + 1, col - 1);
+
+            if (node.right != null)
+                Dfs(node.right, depth + 1, col + 1);
+        }
+
+        Dfs(root, 0, 0);
+
+        return result;
+    }
 }
