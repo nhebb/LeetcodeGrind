a9e473becee509b2f75ac8db867309450d0399bf
Nicholas Hebb <nhebb@breezetree.com>
2023-01-10 00:34:26 -0800

    Added SortedListToBST

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetcodeGrind.LinkedLists;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -592,5 +593,33 @@ public class Trees
         Dfs(root, false);
 
         return sum;
+    }
+
+
+    // 109. Convert Sorted List to Binary Search Tree
+    public TreeNode SortedListToBST(ListNode head)
+    {
+        if (head == null) return null;
+
+        var nums = new List<int>();
+        while (head != null)
+        {
+            nums.Add(head.val);
+            head = head.next;
+        }
+
+        TreeNode CreateTree(int low, int high)
+        {
+            if (high < low)
+                return null;
+
+            var mid = low + (high - low) / 2;
+            var node = new TreeNode(nums[mid]);
+            node.left = CreateTree(low, mid - 1);
+            node.right = CreateTree(mid + 1, high);
+            return node;
+        }
+
+        return CreateTree(0, nums.Count - 1);
     }
 }
