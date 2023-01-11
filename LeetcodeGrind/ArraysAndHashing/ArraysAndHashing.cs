47a62a4dfeb151383f88e2a5ed33ff35f8257746
Nicholas Hebb <nhebb@breezetree.com>
2023-01-11 02:18:24 -0800

    Added HardestWorker

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -1983,4 +1983,29 @@ public class ArraysAndHashing
 
         return min;
     }
+
+
+    // 2432. The Employee That Worked on the Longest Task
+    public int HardestWorker(int n, int[][] logs)
+    {
+        var res = new List<int>() { logs[0][0] };
+        var longest = logs[0][1];
+
+        for (int i = 1; i < logs.Length; i++)
+        {
+            var time = logs[i][1] - logs[i - 1][1];
+            if (time == longest)
+            {
+                res.Add(logs[i][0]);
+            }
+            else if (time > longest)
+            {
+                res.Clear();
+                res.Add(logs[i][0]);
+                longest = time;
+            }
+        }
+
+        return res.Min();
+    }
 }
