c1c8b7d2e54ad674f2099c8bb6cfad948a23f691
Nicholas Hebb <nhebb@breezetree.com>
2023-01-11 01:47:16 -0800

    Added MinTime

--- LeetcodeGrind/Graphs/Graphs.cs
+++ LeetcodeGrind/Graphs/Graphs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetcodeGrind.Trees;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -185,7 +186,7 @@ public class Graphs
             visited.Add(vertex);
             foreach (var neighbor in d[vertex])
             {
-                if(!Dfs(neighbor))
+                if (!Dfs(neighbor))
                     return false;
             }
             d[vertex].Clear();
@@ -222,12 +223,11 @@ public class Graphs
             if (!visited.Add(i))
                 return false;
 
-            var result = true;
             foreach (var neighbor in adj[i])
             {
                 if (neighbor != prev)
                 {
-                    if(! Dfs(neighbor, i))
+                    if (!Dfs(neighbor, i))
                         return false;
                 }
             }
@@ -235,6 +235,67 @@ public class Graphs
         }
 
         return Dfs(0, -1) && visited.Count == n;
-        
+    }
+
+
+    // 1443. Minimum Time to Collect All Apples in a Tree
+    public int MinTime(int n, int[][] edges, IList<bool> hasApple)
+    {
+        if (!hasApple.Any(x => x == true))
+            return 0;
+
+        var children = new Dictionary<int, List<int>>();
+        foreach (var edge in edges)
+        {
+            if (children.TryGetValue(edge[0], out var list1))
+                list1.Add(edge[1]);
+            else
+                children[edge[0]] = new List<int>() { edge[1] };
+
+            if (children.TryGetValue(edge[1], out var list2))
+                list2.Add(edge[0]);
+            else
+                children[edge[1]] = new List<int>() { edge[0] };
+        }
+
+        if (children[0].Count == 0)
+            return 0;
+
+        var time = 0;
+        var visited = new HashSet<int>();
+
+        bool Dfs(int node, int dist)
+        {
+            visited.Add(node);
+            var result = false;
+            if (hasApple[node])
+            {
+                time += dist;
+                dist = 0;
+                result = true;
+            }
+
+            if (children.ContainsKey(node))
+            {
+                foreach (var child in children[node])
+                {
+                    if (visited.Contains(child))
+                        continue;
+
+                    if (Dfs(child, dist + 1))
+                    {
+                        time += 1;
+                        result = true;
+                        dist = 0;
+                    }
+                }
+            }
+            visited.Remove(node);
+            return result;
+        }
+
+        _ = Dfs(0, 0);
+
+        return time;
     }
 }
--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -4,19 +4,21 @@
     {
         static void Main(string[] args)
         {
-            /*length =
-            
-            width =
-            
-            height =
-            
-            mass =
-            
-            */
+            var g = new Graphs.Graphs();
+            var edges = new int[6][];
+            edges[0] = new int[] { 0, 1 };
+            edges[1] = new int[] { 0, 2 };
+            edges[2] = new int[] { 1, 4 };
+            edges[3] = new int[] { 1, 5 };
+            edges[4] = new int[] { 2, 3 };
+            edges[5] = new int[] { 2, 6 };
 
-            var sw = new SlidingWindow.SlidingWindow();
-            var nums = new int[] { 1, 1, 2, 1, 1 };
-            var res = sw.NumberOfSubarrays(nums, 3);
+            var hasApple = new List<bool>()
+            {
+                false, false, true, false, true, true, false
+            };
+
+            var res = g.MinTime(7, edges, hasApple);
 
             Console.WriteLine(res);
         }
