49a5c4206875b3edb707dfd66bdd8eaac820b76d
Nicholas Hebb <nhebb@breezetree.com>
2023-01-16 03:03:49 -0800

    Added OddCells

--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -358,4 +358,31 @@ public class Matrices
         return moves.Count(x => x == 'L') == moves.Count(x => x == 'R')
             && moves.Count(x => x == 'U') == moves.Count(x => x == 'D');
     }
+
+
+    // 1252. Cells with Odd Values in a Matrix
+    public int OddCells(int m, int n, int[][] indices)
+    {
+        var rows = new int[m];
+        var cols = new int[n];
+
+        foreach (var index in indices)
+        {
+            rows[index[0]]++;
+            cols[index[1]]++;
+        }
+
+        var count = 0;
+
+        for (int r = 0; r < m; r++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                if ((rows[r] + cols[c]) % 2 == 1)
+                    count++;
+            }
+        }
+
+        return count;
+    }
 }
