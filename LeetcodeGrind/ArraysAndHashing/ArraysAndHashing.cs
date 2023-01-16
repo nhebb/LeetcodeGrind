29e165c39d474103ab6f5f295aacd0390353d3c7
Nicholas Hebb <nhebb@breezetree.com>
2023-01-15 18:40:37 -0800

    Added DifferenceOfSum

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -2025,7 +2025,7 @@ public class ArraysAndHashing
         return ans[n];
     }
 
-
+    // TODO: finish
     // 2537. Count the Number of Good Subarrays
     public long CountGood(int[] nums, int k)
     {
@@ -2051,5 +2051,26 @@ public class ArraysAndHashing
             }
         }
         return count;
+    }
+
+
+    // 2535. Difference Between Element Sum and Digit Sum of an Array
+    public int DifferenceOfSum(int[] nums)
+    {
+        var sumNums = 0;
+        var sumDigits = 0;
+
+        foreach (var num in nums)
+        {
+            sumNums += num;
+            var n = num;
+            while (n > 0)
+            {
+                sumDigits += n % 10;
+                n /= 10;
+            }
+        }
+
+        return Math.Abs(sumNums - sumDigits);
     }
 }
