ff45ee51ecc719443f9c82ce44668334d3cd827f
Nicholas Hebb <nhebb@breezetree.com>
2023-01-16 03:11:33 -0800

    Added IncreasingBST

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -803,4 +803,37 @@ public class Trees
         Dfs(root, 0);
         return ans;
     }
+
+
+    // 897. Increasing Order Search Tree
+    public TreeNode IncreasingBST(TreeNode root)
+    {
+        if (root == null)
+            return root;
+
+        var nodes = new List<TreeNode>();
+
+        void CreateList(TreeNode node)
+        {
+            if (node.left != null)
+                CreateList(node.left);
+
+            nodes.Add(node);
+
+            if (node.right != null)
+                CreateList(node.right);
+        }
+
+        CreateList(root);
+        for (int i = 0; i < nodes.Count - 1; i++)
+        {
+            nodes[i].left = null;
+            nodes[i].right = nodes[i + 1];
+        }
+
+        nodes[^1].left = null;
+        nodes[^1].right = null;
+
+        return nodes[0];
+    }
 }
