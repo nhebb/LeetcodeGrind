0626919d8ddf84eeb7bd4ce2ef052a464be4583f
Nicholas Hebb <nhebb@breezetree.com>
2023-01-16 03:46:14 -0800

    Added FreqAlphabets

--- LeetcodeGrind/Strings/Strings.cs
+++ LeetcodeGrind/Strings/Strings.cs
@@ -598,4 +598,26 @@ public class Strings
 
         return "";
     }
+
+
+    // 1309. Decrypt String from Alphabet to Integer Mapping
+    public string FreqAlphabets(string s)
+    {
+        int val = 0;
+        var sb = new StringBuilder();
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '#')
+            {
+                val = 10 * (s[i - 2] - '0') + (s[i - 1] - '0');
+                i -= 2;
+            }
+            else
+            {
+                val = s[1] - '0';
+            }
+            sb.Append((char)('a' + val - 1));
+        }
+        return string.Join("", sb.ToString().Reverse());
+    }
 }
