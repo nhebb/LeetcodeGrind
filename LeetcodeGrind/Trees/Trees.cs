a33329b36c79880cbb496bea86cb3e56ae6550e1
Nicholas Hebb <nhebb@breezetree.com>
2023-01-16 21:16:28 -0800

    Added NumTrees

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -836,4 +836,21 @@ public class Trees
 
         return nodes[0];
     }
+
+
+    // 96. Unique Binary Search Trees
+    public int NumTrees(int n)
+    {
+        var dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
 }
