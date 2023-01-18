bfbe812448b49834be0fcaf245b021d50886a6c2
Nicholas Hebb <nhebb@breezetree.com>
2023-01-18 02:23:46 -0800

    Added MostCommonWord

--- LeetcodeGrind/Strings/Strings.cs
+++ LeetcodeGrind/Strings/Strings.cs
@@ -621,10 +621,39 @@ public class Strings
         return string.Join("", sb.ToString().Reverse());
     }
 
-    //TODO: finish
-    // 1641. Count Sorted Vowel Strings
-    //public int CountVowelStrings(int n)
-    //{
 
-    //}
+    //819. Most Common Word
+    public string MostCommonWord(string paragraph, string[] banned)
+    {
+        Array.Sort(banned);
+        var splitChars = " !?',;.".ToCharArray();
+
+        var words = paragraph.ToLower()
+                             .Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+
+        var wordCounts = new Dictionary<string, int>();
+        foreach (var word in words)
+        {
+            if (Array.BinarySearch<string>(banned, word) < 0)
+            {
+                if (wordCounts.ContainsKey(word))
+                    wordCounts[word]++;
+                else
+                    wordCounts[word] = 1;
+            }
+        }
+
+        var maxFreq = 0;
+        var maxFreqWord = "";
+
+        foreach (var kvp in wordCounts)
+        {
+            if (kvp.Value > maxFreq)
+            {
+                maxFreq = kvp.Value;
+                maxFreqWord = kvp.Key;
+            }
+        }
+        return maxFreqWord;
+    }
 }
