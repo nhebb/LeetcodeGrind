0c3cfee06eac27ee71e3a526ae9be0c7504d7619
Nicholas Hebb <nhebb@breezetree.com>
2023-01-17 18:37:37 -0800

    Added GetRow

--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -6,17 +6,14 @@ namespace LeetcodeGrind
     {
         static void Main(string[] args)
         {
-            var heights = new int[5][];
-            heights[0] = new int[] { 1, 2, 2, 3, 5 };
-            heights[1] = new int[] { 3, 2, 3, 4, 4 };
-            heights[2] = new int[] { 2, 4, 5, 3, 1 };
-            heights[3] = new int[] { 6, 7, 1, 4, 5 };
-            heights[4] = new int[] { 5, 1, 1, 2, 4 };
+            var dp = new DynamicProgramming.DynamicProgramming();
 
-            var g = new Graphs.Graphs();
-            var res = g.PacificAtlantic(heights);
-
-            Console.WriteLine(string.Join(",", res));
+            for (int i = 0; i <= 5; i++)
+            {
+                var res = dp.GetRow(i);
+                Console.WriteLine(string.Join(",", res));
+                Console.WriteLine();
+            }
         }
     }
 }
