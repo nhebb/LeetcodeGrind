c8dde94ec76611e1808f70c7da4c2c39d847e736
Nicholas Hebb <nhebb@breezetree.com>
2023-01-19 06:08:16 -0800

    Added SubarraysDivByK

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -2280,4 +2280,33 @@ public class ArraysAndHashing
         }
         return minLen;
     }
+
+
+    // 974. Subarray Sums Divisible by K
+    public int SubarraysDivByK(int[] nums, int k)
+    {
+        var count = 0;
+        var curr = 0;
+        var prefMap = new Dictionary<int, int>();
+        prefMap[0] = 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            curr += nums[i];
+            curr %= k;
+            if (curr < 0)
+                curr += k;
+
+            if (prefMap.ContainsKey(curr))
+            {
+                count += prefMap[curr];
+                prefMap[curr] += 1;
+            }
+            else
+            {
+                prefMap[curr] = 1;
+            }
+        }
+        return count;
+    }
 }
