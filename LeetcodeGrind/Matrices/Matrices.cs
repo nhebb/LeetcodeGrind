75ea698bd095dc8fd6857e785ff7f19b9daecd1a
Nicholas Hebb <nhebb@breezetree.com>
2023-01-20 16:21:34 -0800

    Added FindSubsequences and RangeAddQueries

--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -385,4 +385,26 @@ public class Matrices
 
         return count;
     }
+
+
+    // 2536. Increment Submatrices by One
+    public int[][] RangeAddQueries(int n, int[][] queries)
+    {
+        var ans = new int[n][];
+        for (int i = 0; i < n; i++)
+            ans[i] = new int[n];
+
+        foreach (var query in queries)
+        {
+            for (int r = query[0]; r <= query[2]; r++)
+            {
+                for (int c = query[1]; c <= query[3]; c++)
+                {
+                    ans[r][c] += 1;
+                }
+            }
+        }
+
+        return ans;
+    }
 }
