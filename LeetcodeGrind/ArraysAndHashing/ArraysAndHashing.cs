e5ae224ede22a45f6329d38ccc319fce09aa7a02
Nicholas Hebb <nhebb@breezetree.com>
2023-01-24 17:13:23 -0800

    Added MajorityElementII

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -2460,4 +2460,24 @@ public class ArraysAndHashing
 
         return ans;
     }
+
+
+    // 229. Majority Element II
+    public IList<int> MajorityElementII(int[] nums)
+    {
+        var numCount = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (!numCount.TryAdd(num, 1))
+            {
+                numCount[num]++;
+            }
+        }
+
+        var ans = new List<int>();
+        var target = nums.Length / 3;
+        return numCount.Where(x => x.Value > target)
+                       .Select(x => x.Key)
+                       .ToList();
+    }
 }
