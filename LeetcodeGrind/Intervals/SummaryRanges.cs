686c3d58bbd52cf79fb95c04922fdd4c67d93ca1
Nicholas Hebb <nhebb@breezetree.com>
2023-01-28 07:03:06 -0800

    It's annoting that VS's git interface lets you commit changes in an unsaved file, so you have to save and commit again.

--- LeetcodeGrind/Intervals/SummaryRanges.cs
+++ LeetcodeGrind/Intervals/SummaryRanges.cs
@@ -44,23 +44,24 @@ public class SummaryRanges
     }
 }
 
+// Note: using a List instead of a HashSet ended up with the same performance
 public class SummaryRanges2
 {
-    private List<int> list;
+    private List<int> nums;
 
     public SummaryRanges2()
     {
-        list = new List<int>();
+        nums = new List<int>();
     }
 
     public void AddNum(int value)
     {
-        list.Add(value);
+        nums.Add(value);
     }
 
     public int[][] GetIntervals()
     {
-        var nums = list.OrderBy(x => x).ToList();
+        nums.Sort();
         var ans = new List<int[]>();
 
         for (int i = 0; i < nums.Count; i++)
@@ -68,7 +69,7 @@ public class SummaryRanges2
             var firstVal = nums[i];
             var lastVal = firstVal;
             int j = i + 1;
-            while (j < nums.Count && nums[j] == lastVal + 1)
+            while (j < nums.Count && nums[j] <= lastVal + 1)
             {
                 lastVal = nums[j];
                 j++;
