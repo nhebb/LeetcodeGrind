d5fddea84af7a84e429d4f8b921b210c603de63e
Nicholas Hebb <nhebb@breezetree.com>
2023-01-28 07:43:24 -0800

    Added LevelOrderBottom

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -866,4 +866,37 @@ public class Trees
 
         return SearchBST(root.right, val);
     }
+
+
+    // 107. Binary Tree Level Order Traversal II
+    public IList<IList<int>> LevelOrderBottom(TreeNode root)
+    {
+        var d = new Dictionary<int, IList<int>>();
+        var maxLevel = -0;
+
+        void Dfs(TreeNode node, int level)
+        {
+            maxLevel = Math.Max(maxLevel, level);
+
+            if (!d.ContainsKey(level))
+                d[level] = new List<int>();
+            d[level].Add(node.val);
+
+            if (node.left != null)
+                Dfs(node.left, level + 1);
+            if (node.right != null)
+                Dfs(node.right, level + 1);
+        }
+
+        var ans = new List<IList<int>>();
+        if (root == null)
+            return ans;
+
+        Dfs(root, 0);
+
+        for (int i = maxLevel; i >= 0; i--)
+            ans.Add(d[i]);
+
+        return ans;
+    }
 }
