94f2fa06b12fcef4ef95371a2c0a5f2177d0d60c
Nicholas Hebb <nhebb@breezetree.com>
2023-02-03 02:24:59 -0800

    Added HIndex and HIndexII

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -2544,5 +2544,46 @@ public class ArraysAndHashing
             xor ^= num;
         return xor;
     }
+
+
+    // 274. H-Index
+    public int HIndex(int[] citations)
+    {
+        Array.Sort(citations);
+        int i = 0;
+        int j = citations.Length - 1;
+
+        while (i <= j)
+        {
+            int m = i + (j - i) / 2;
+
+            if (citations[m] >= citations.Length - m)
+                j = m - 1;
+            else
+                i = m + 1;
+        }
+
+        return citations.Length - i;
+    }
+
+
+    // 275. H-Index II
+    public int HIndexII(int[] citations)
+    {
+        int i = 0;
+        int j = citations.Length - 1;
+
+        while (i <= j)
+        {
+            int m = i + (j - i) / 2;
+
+            if (citations[m] >= citations.Length - m)
+                j = m - 1;
+            else
+                i = m + 1;
+        }
+
+        return citations.Length - i;
+    }
 }
 
