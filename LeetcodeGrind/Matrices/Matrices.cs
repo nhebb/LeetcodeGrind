9b245295a551a7841429161378b0ef9fc7642dad
Nicholas Hebb <nhebb@breezetree.com>
2023-02-06 17:48:55 -0800

    Added FindDiagonalOrder

--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -503,4 +503,56 @@ public class Matrices
         }
         return -1;
     }
+
+
+    // 498. Diagonal Traverse
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        var n = mat.Length * mat[0].Length;
+        var ans = new List<int>(n);
+
+        var bottom = mat.Length - 1;
+        var right = mat[0].Length - 1;
+        var startPos = new int[bottom + right + 1, 2];
+
+        for (int i = 0; i < startPos.GetLength(0); i++)
+        {
+            if (i % 2 == 0)
+            {
+                startPos[i, 0] = Math.Min(i, bottom);
+                startPos[i, 1] = Math.Max(0, i - bottom);
+            }
+            else
+            {
+                startPos[i, 0] = Math.Max(0, i - right);
+                startPos[i, 1] = Math.Min(i, right);
+            }
+        }
+
+        for (int i = 0; i < startPos.GetLength(0); i++)
+        {
+            var r = startPos[i, 0];
+            var c = startPos[i, 1];
+            if (i % 2 == 0)
+            {
+                while (r >= 0 && c <= right)
+                {
+                    ans.Add(mat[r][c]);
+                    r--;
+                    c++;
+                }
+            }
+            else
+            {
+                while (r <= bottom && c >= 0)
+                {
+                    ans.Add(mat[r][c]);
+                    r++;
+                    c--;
+                }
+            }
+        }
+
+        return ans.ToArray();
+    }
 }
--- LeetcodeGrind/Program.cs
+++ LeetcodeGrind/Program.cs
@@ -6,11 +6,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var s = "abacbabc";
-        var p = "abc";
-        var ah = new ArraysAndHashing.ArraysAndHashing();
-        var res = ah.FindAnagrams(s, p);
+        var mat = new int[3][];
+        mat[0] = new int[] { 1, 2, 3 };
+        mat[1] = new int[] { 4, 5, 6 };
+        mat[2] = new int[] { 7, 8, 9 };
 
-        Console.WriteLine(string.Join(",",res));
+        var m = new Matrices.Matrices();
+        var res = m.FindDiagonalOrder2(mat);
+
+        Console.WriteLine(string.Join(",", res));
     }
 }
