06716c490b947c7ac2807a52dcdf875106b17a41
Nicholas Hebb <nhebb@breezetree.com>
2023-02-06 17:57:32 -0800

    Modified FindDiagonalOrder

--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -508,25 +508,19 @@ public class Matrices
     // 498. Diagonal Traverse
     public int[] FindDiagonalOrder(int[][] mat)
     {
-        var n = mat.Length * mat[0].Length;
-        var ans = new List<int>(n);
-
+        var ans = new List<int>(mat.Length * mat[0].Length);
         var bottom = mat.Length - 1;
         var right = mat[0].Length - 1;
         var startPos = new int[bottom + right + 1, 2];
 
         for (int i = 0; i < startPos.GetLength(0); i++)
         {
-            if (i % 2 == 0)
-            {
-                startPos[i, 0] = Math.Min(i, bottom);
-                startPos[i, 1] = Math.Max(0, i - bottom);
-            }
-            else
-            {
-                startPos[i, 0] = Math.Max(0, i - right);
-                startPos[i, 1] = Math.Min(i, right);
-            }
+            startPos[i, 0] = i % 2 == 0
+                ? Math.Min(i, bottom)
+                : Math.Max(0, i - right);
+            startPos[i, 1] = i % 2 == 0
+                ? Math.Max(0, i - bottom)
+                : Math.Min(i, right);
         }
 
         for (int i = 0; i < startPos.GetLength(0); i++)
@@ -534,25 +528,13 @@ public class Matrices
             var r = startPos[i, 0];
             var c = startPos[i, 1];
             if (i % 2 == 0)
-            {
                 while (r >= 0 && c <= right)
-                {
-                    ans.Add(mat[r][c]);
-                    r--;
-                    c++;
-                }
-            }
+                    ans.Add(mat[r--][c++]);
             else
-            {
                 while (r <= bottom && c >= 0)
-                {
-                    ans.Add(mat[r][c]);
-                    r++;
-                    c--;
-                }
-            }
-        }
+                    ans.Add(mat[r++][c--]);
 
+        }
         return ans.ToArray();
     }
 }
