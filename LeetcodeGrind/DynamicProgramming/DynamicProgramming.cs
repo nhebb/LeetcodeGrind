7ec2add050ed7f47385e1275d397b7e5c4f56263
Nicholas Hebb <nhebb@breezetree.com>
2023-02-08 06:00:15 -0800

    Added MinCostClimbingStairs

--- LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
+++ LeetcodeGrind/DynamicProgramming/DynamicProgramming.cs
@@ -481,4 +481,23 @@ public class DynamicProgramming
 
         return false;
     }
+
+
+    // 746. Min Cost Climbing Stairs
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        if (cost.Length == 2)
+            return Math.Min(cost[0], cost[1]);
+
+        var dp = new int[cost.Length];
+        dp[^1] = cost[^1];
+        dp[^2] = cost[^2];
+
+        for (int i = dp.Length - 3; i >= 0; i--)
+        {
+            dp[i] = Math.Min(cost[i] + dp[i + 1], cost[i] + dp[i + 2]);
+        }
+
+        return Math.Min(dp[0], dp[1]);
+    }
 }
