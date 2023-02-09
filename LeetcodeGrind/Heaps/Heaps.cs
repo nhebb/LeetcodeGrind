b883266021c762186c9e365717c112f57d89ad99
Nicholas Hebb <nhebb@breezetree.com>
2023-02-09 02:23:47 -0800

    Added PickGifts

--- LeetcodeGrind/Heaps/Heaps.cs
+++ LeetcodeGrind/Heaps/Heaps.cs
@@ -293,4 +293,27 @@ public class Heaps
 
         return pq.Dequeue();
     }
+
+
+    // 2558. Take Gifts From the Richest Pile
+    public long PickGifts(int[] gifts, int k)
+    {
+        var pq = new PriorityQueue<int, int>();
+        foreach (var gift in gifts)
+            pq.Enqueue(gift, -gift);
+
+        while (k > 0)
+        {
+            var qty = pq.Dequeue();
+            var putback = (int)(Math.Floor(Math.Sqrt(qty)));
+            pq.Enqueue(putback, -putback);
+            k--;
+        }
+
+        long remaining = 0;
+        while (pq.Count > 0)
+            remaining += pq.Dequeue();
+
+        return remaining;
+    }
 }
