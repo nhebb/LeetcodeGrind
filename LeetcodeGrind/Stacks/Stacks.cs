5e1fc34e779b118324007793d9090c5d09dac44e
Nicholas Hebb <nhebb@breezetree.com>
2023-02-14 00:05:30 -0800

    Added LargestRectangleArea

--- LeetcodeGrind/Stacks/Stacks.cs
+++ LeetcodeGrind/Stacks/Stacks.cs
@@ -92,9 +92,42 @@ public class Stacks
     }
 
 
+    // TODO: Finish (or at least get started)
     // 402. Remove K Digits
     public string RemoveKdigits(string num, int k)
     {
         return num;
+    }
+
+
+    // 84. Largest Rectangle in Histogram
+    public int LargestRectangleArea(int[] heights)
+    {
+        var stack = new Stack<int[]>();
+        var maxArea = 0;
+
+        for (int i = 1; i < heights.Length; i++)
+        {
+            var startIndex = i;
+            while (stack.Count > 0 && stack.Peek()[1] > heights[i])
+            {
+                var rect = stack.Pop();
+                var index = rect[0];
+                var height = rect[1];
+                maxArea = Math.Max(maxArea, height * (i - index));
+                startIndex = index;
+            }
+            stack.Push(new int[] { startIndex, heights[i] });
+        }
+
+        while (stack.Count > 0)
+        {
+            var rect = stack.Pop();
+            var index = rect[0];
+            var height = rect[1];
+            maxArea = Math.Max(maxArea, height * (heights.Length - index);
+        }
+
+        return maxArea;
     }
 }
