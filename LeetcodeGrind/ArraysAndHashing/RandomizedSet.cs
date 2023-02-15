97dee34bcc18f7ac709544045a2eaf23c2129e3a
Nicholas Hebb <nhebb@breezetree.com>
2023-02-15 00:36:13 -0800

    Added GridGame and RandomizedSet

---
+++ LeetcodeGrind/ArraysAndHashing/RandomizedSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeGrind.ArraysAndHashing;
+
+public class RandomizedSet
+{
+    private HashSet<int> _hs;
+    private Random _random;
+
+    public RandomizedSet()
+    {
+        _hs = new HashSet<int>();
+        _random = new Random();
+    }
+
+    public bool Insert(int val)
+    {
+        return _hs.Add(val);
+    }
+
+    public bool Remove(int val)
+    {
+        return _hs.Remove(val);
+    }
+
+    public int GetRandom()
+    {
+        return _hs.ElementAt(_random.Next(_hs.Count - 1));
+    }
+}
--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -680,4 +680,30 @@ public class Matrices
 
         return ans;
     }
+
+
+    // 2017. Grid Game
+    public long GridGame(int[][] grid)
+    {
+        var pre = new long[grid[0].Length];
+        pre[0] = grid[0][0];
+        for (int c = 1; c < grid[0].Length; c++)
+            pre[c] = pre[c - 1] + grid[0][c];
+
+        var post = new long[grid[0].Length];
+        post[^1] = grid[1][^1];
+        for (int c = grid[1].Length - 2; c >= 0; c--)
+            post[c] = post[c + 1] + grid[1][c];
+
+        long min = long.MaxValue;
+        var i = 0;
+        while (i < grid[0].Length)
+        {
+            min = Math.Min(min,
+                Math.Max(pre[^1] - pre[i], post[0] - post[i]));
+            i++;
+        }
+
+        return min;
+    }
 }
