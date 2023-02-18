b3a6ff09a48021b60f6a57ed341e5f4ff8e1547a
Nicholas Hebb <nhebb@breezetree.com>
2023-02-17 23:25:54 -0800

    Added MinDiffInBST

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -936,4 +936,33 @@ public class Trees
 
         return root;
     }
+
+
+    // 783. Minimum Distance Between BST Nodes
+    public int MinDiffInBST(TreeNode root)
+    {
+        var vals = new List<int>();
+        void Dfs(TreeNode node)
+        {
+            if (node.left != null)
+                Dfs(node.left);
+
+            vals.Add(node.val);
+
+            if (node.right != null)
+                Dfs(node.right);
+        }
+
+        Dfs(root);
+
+        var minDiff = vals[1] - vals[0];
+        for (int i = 2; i < vals.Count; i++)
+        {
+            var diff = vals[i] - vals[i - 1];
+            if (diff < minDiff)
+                minDiff = diff;
+        }
+        return minDiff;
+    }
+
 }
