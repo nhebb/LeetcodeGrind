3c87eb0d6276f31e3b11ad3fea75e8b6efa0f55e
Nicholas Hebb <nhebb@breezetree.com>
2023-02-18 04:39:26 -0800

    Added RemoveLeafNodes

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -965,4 +965,38 @@ public class Trees
         return minDiff;
     }
 
+
+    // 1325. Delete Leaves With a Given Value
+    public TreeNode RemoveLeafNodes(TreeNode root, int target)
+    {
+        TreeNode DeleteLeaf(TreeNode node)
+        {
+            if (node == null) 
+                return null;
+
+            node.left = DeleteLeaf(node.left);
+            node.right = DeleteLeaf(node.right);
+
+            if (node.val == target
+                && node.left == null
+                && node.right == null)
+            {
+                return null;
+            }
+
+            return node;
+        }
+
+        root.left = DeleteLeaf(root.left);
+        root.right = DeleteLeaf(root.right);
+
+        if (root.val == target
+            && root.left == null
+            && root.right == null)
+        {
+            return null;
+        }
+
+        return root;
+    }
 }
