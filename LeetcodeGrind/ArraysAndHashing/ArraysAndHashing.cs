e127adc5fd0efb4a5fd5b849f3edf89e2ce41779
Nicholas Hebb <nhebb@breezetree.com>
2023-02-18 04:11:54 -0800

    Added MinSteps and DeckRevealedIncreasing

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -3094,4 +3094,55 @@ public class ArraysAndHashing
         }
         return q.Dequeue();
     }
+
+
+    // 950. Reveal Cards in Increasing Order
+    public int[] DeckRevealedIncreasing(int[] deck)
+    {
+        Array.Sort(deck);
+        var q = new Queue<int>(Enumerable.Range(0, deck.Length));
+        var res = new int[deck.Length];
+
+        foreach (var card in deck)
+        {
+            res[q.Dequeue()] = card;
+            if (q.Count > 0)
+                q.Enqueue(q.Dequeue());
+        }
+        return res;
+    }
+
+
+    // 1347. Minimum Number of Steps to Make Two Strings Anagram
+    public int MinSteps(string s, string t)
+    {
+        var ds = new Dictionary<char, int>();
+        var dt = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (ds.TryGetValue(s[i], out var val))
+                ds[s[i]] = val + 1;
+            else
+                ds[s[i]] = 1;
+        }
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (dt.TryGetValue(t[i], out var val))
+                dt[t[i]] = val + 1;
+            else
+                dt[t[i]] = 1;
+        }
+
+        var need = 0;
+        foreach (var kvp in ds)
+        {
+            if (dt.ContainsKey(kvp.Key))
+                need += Math.Max(0, kvp.Value - dt[kvp.Key]);
+            else
+                need += kvp.Value;
+        }
+
+        return need;
+    }
 }
