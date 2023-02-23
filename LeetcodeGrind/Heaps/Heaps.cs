4825479903466edb268e3b04830326bbf8f81d62
Nicholas Hebb <nhebb@breezetree.com>
2023-02-23 01:02:31 -0800

    Added FindMaximizedCapital solutions

--- LeetcodeGrind/Heaps/Heaps.cs
+++ LeetcodeGrind/Heaps/Heaps.cs
@@ -368,4 +368,54 @@ public class Heaps
 
         return w;
     }
+    // Faster version with HashSet instead of Queue
+    public int FindMaximizedCapital2(int k, int w, int[] profits, int[] capital)
+    {
+        var pq = new PriorityQueue<int, int>();
+        var hs = new HashSet<int>();
+        var hsDelete = new HashSet<int>();
+
+        // If the project capital is <= the initial w value
+        // then put it in the priority queue (min heap),
+        // otherwise put its index in a hashset for subsequent
+        // iterations.
+        for (int i = 0; i < capital.Length; i++)
+        {
+            if (capital[i] <= w)
+                pq.Enqueue(profits[i], -profits[i]);
+            else
+                hs.Add(i);
+        }
+
+        while (k > 0)
+        {
+            // Edge case, there are less projects that Leetcode
+            // has the capital for than the remaining k value
+            if (pq.Count == 0)
+                break;
+
+            // Increment capital with the profit
+            w += pq.Dequeue();
+            k--;
+            if (k == 0) break;
+
+            // By using a hashset, each time we remove a project
+            // and place it in the priority queue, we have less
+            // items to cycle over in subsequent iterations.
+            foreach (var i in hs)
+            {
+                if (capital[i] <= w)
+                {
+                    pq.Enqueue(profits[i], -profits[i]);
+                    hsDelete.Add(i);
+                }
+            }
+            foreach(var i in hsDelete)
+                hs.Remove(i);
+            hsDelete.Clear();
+        }
+
+        return w;
+    }
+
 }
