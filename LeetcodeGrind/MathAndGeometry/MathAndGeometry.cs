73dd30d38ff6f9d1c21620eeec10d7fa1be9275d
Nicholas Hebb <nhebb@breezetree.com>
2023-02-22 18:18:00 -0800

    Added MinOperations

--- LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
+++ LeetcodeGrind/MathAndGeometry/MathAndGeometry.cs
@@ -675,20 +675,36 @@ public class MathAndGeometry
         return divisors.Sum() == num;
     }
 
-    int GetMostSignificantBit(int n)
+
+    // 2571. Minimum Operations to Reduce an Integer to 0
+    public int MinOperations(int n)
     {
-        if (n == 0)
-            return 0;
-
-        int msb = 0;
-        n /= 2;
-
-        while (n != 0)
+        int GetMostSignificantBit(int n)
         {
+            if (n == 0)
+                return 0;
+
+            int msb = 0;
             n /= 2;
-            msb++;
+
+            while (n != 0)
+            {
+                n /= 2;
+                msb++;
+            }
+
+            return (1 << msb);
         }
 
-        return (1 << msb);
+        int GetHammingWeight(int n)
+        {
+            var binary = Convert.ToString(n, 2);
+            return binary.Count(c => c== '1');
+        }
+        
+        var val1 = GetHammingWeight((GetMostSignificantBit(n) * 2)) + 1;
+        var val2 = GetHammingWeight(n);
+
+        return Math.Min(val1, val2);
     }
 }
