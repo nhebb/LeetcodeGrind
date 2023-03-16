8b2481ae24c531268eb65cbb02cfe4bfc0a7e10a
Nicholas Hebb <nhebb@breezetree.com>
2023-03-16 03:27:00 -0700

    Added BuildTree2

--- LeetcodeGrind/Trees/Trees.cs
+++ LeetcodeGrind/Trees/Trees.cs
@@ -1293,4 +1293,34 @@ public class Trees
 
         return true;
     }
+
+
+    // 106. Construct Binary Tree from Inorder and Postorder Traversal
+    public TreeNode BuildTree2(int[] inorder, int[] postorder)
+    {
+        // Base constraints state that there will be at least 1
+        // node, but required for recursive calls.
+        if (inorder.Length == 0 && postorder.Length == 0) 
+            return null;
+
+        // The last node in a post order traversal
+        // is the tree's root.
+        var root = new TreeNode(postorder[^1]);
+
+        if (inorder.Length == 1) 
+            return root;
+
+        int rootIndex = Array.IndexOf(inorder, root.val);
+
+        var inorderLeft = inorder.Take(rootIndex).ToArray();
+        var postorderLeft = postorder.Take(rootIndex).ToArray();
+
+        var inorderRight = inorder.Skip(rootIndex + 1).ToArray();
+        var postorderRight = postorder.Skip(rootIndex).Take(inorderRight.Length).ToArray();
+
+        root.left = BuildTree(inorderLeft, postorderLeft);
+        root.right = BuildTree(inorderRight, postorderRight);
+
+        return root;
+    }
 }
