87f58151163a8437445919a0bd63a4fb8678121d
Nicholas Hebb <nhebb@breezetree.com>
2023-04-18 15:07:27 -0700

    Added UpdateMatrix and KidsWithCandies

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -3660,4 +3660,23 @@ public class ArraysAndHashing
         }
         return count;
     }
+
+
+    // 1431. Kids With the Greatest Number of Candies
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var maxCandies = candies[0];
+        for (int i = 1; i < candies.Length; i++)
+        {
+            maxCandies = Math.Max(candies[i], maxCandies);
+        }
+
+        var ans = new bool[candies.Length];
+        for (int i = 0; i < candies.Length; i++)
+        {
+            ans[i] = candies[i] + extraCandies >= maxCandies;
+        }
+
+        return ans;
+    }
 }
--- LeetcodeGrind/Matrices/Matrices.cs
+++ LeetcodeGrind/Matrices/Matrices.cs
@@ -863,55 +863,66 @@ public class Matrices
         for (int r = 0; r < rows; r++)
         {
             ans[r] = new int[cols];
-            Array.Fill(ans[r], int.MaxValue);
         }
 
-        // initialize ans with 0's
+        // Initialize ans matrix with 0's for 0 values in mat.
+        // Set 1 values in mat to int.MaxValue in ans for comparison
+        // when doing BFS.
+        var q = new Queue<(int row, int col)>();
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < cols; c++)
             {
                 if (mat[r][c] == 0)
-                    ans[r][c] = 0;
-            }
-        }
-
-        var target = 0;
-        var updated = true;
-        var visited = new HashSet<(int, int)>();
-
-        bool UpdateCell(int r, int c, int dist)
-        {
-            if (r < 0 || r == rows) return false;
-            if (c < 0 || c == cols) return false;
-            if (ans[r][c] <= dist) return false;
-            if (visited.Contains((r, c))) return false;
-
-            ans[r][c] = Math.Min(ans[r][c], dist + 1);
-            visited.Add((r, c));
-
-            return true;
-        }
-
-        while (updated)
-        {
-            updated = false;
-
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < cols; c++)
                 {
-                    if (mat[r][c] != target)
-                        continue;
-
-                    updated |= UpdateCell(r - 1, c, target);
-                    updated |= UpdateCell(r + 1, c, target);
-                    updated |= UpdateCell(r, c - 1, target);
-                    updated |= UpdateCell(r, c + 1, target);
+                    ans[r][c] = 0;
+                    q.Enqueue((r, c));
+                }
+                else
+                {
+                    ans[r][c] = int.MaxValue;
                 }
             }
-            target++;
-            visited.Clear();
+        }
+
+        // BFS over matrix. Start with 0 vals and check neighboring cells.
+        // If the new distance is less than the existing val in ans, then
+        // update the ans cell value and enqueue the cell.
+        while (q.Count > 0)
+        {
+            var count = q.Count;
+            for (int i = 0; i < q.Count; i++)
+            {
+                var cell = q.Dequeue();
+                var r = cell.row;
+                var c = cell.col;
+                var dist = ans[r][c] + 1;
+
+                // top neighbor
+                if (r > 0 && dist < ans[r - 1][c])
+                {
+                    ans[r - 1][c] = dist;
+                    q.Enqueue((r - 1, c));
+                }
+                // bottom neighbor
+                if (r < rows - 1 && dist < ans[r + 1][c])
+                {
+                    ans[r + 1][c] = dist;
+                    q.Enqueue((r + 1, c));
+                }
+                // left neighbor
+                if (c > 0 && dist < ans[r][c - 1])
+                {
+                    ans[r][c - 1] = dist;
+                    q.Enqueue((r, c - 1));
+                }
+                // right neighbor
+                if (c < cols - 1 && dist < ans[r][c + 1])
+                {
+                    ans[r][c + 1] = dist;
+                    q.Enqueue((r, c + 1));
+                }
+            }
         }
 
         return ans;
