da9c757140dccaf2539c3972aa75e99f90bd01ee
Nicholas Hebb <nhebb@breezetree.com>
2023-04-23 01:04:36 -0700

    Added GetConcatenation

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -3679,4 +3679,28 @@ public class ArraysAndHashing
 
         return ans;
     }
+
+
+    // 1929. Concatenation of Array
+    public int[] GetConcatenation(int[] nums)
+    {
+        var n = nums.Length;
+        var ans = new int[n * 2];
+
+        // slowest
+        //Array.Copy(nums, 0, ans, 0, n);
+        //Array.Copy(nums, 0, ans, n, n);
+
+        // slower
+        //nums.CopyTo(ans, 0);
+        //nums.CopyTo(ans, n);
+
+        // faster
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
 }
