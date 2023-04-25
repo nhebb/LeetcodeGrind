efc32860eda0c398d0928e392eda5e35ed62dcc5
Nicholas Hebb <nhebb@breezetree.com>
2023-04-24 17:45:07 -0700

    Saved SmallestInfiniteSet

--- LeetcodeGrind/Heaps/SmallestInfiniteSet.cs
+++ LeetcodeGrind/Heaps/SmallestInfiniteSet.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetcodeGrind.Heaps
+namespace LeetcodeGrind.Heaps;
+
+// 2336. Smallest Number in Infinite Set
+public class SmallestInfiniteSet
 {
-    internal class SmallestInfiniteSet
+    HashSet<int> removed;
+    PriorityQueue<int, int> pq;
+
+    public SmallestInfiniteSet()
     {
+        removed = new HashSet<int>();
+        pq = new PriorityQueue<int, int>();
+        for (int i = 1; i <= 1001; i++)
+            pq.Enqueue(i, i);
+    }
+
+    public int PopSmallest()
+    {
+        var num = pq.Dequeue();
+        removed.Add(num);
+        return num;
+    }
+
+    public void AddBack(int num)
+    {
+        if (removed.Contains(num))
+        {
+            removed.Remove(num);
+            pq.Enqueue(num, num);
+        }
     }
 }
