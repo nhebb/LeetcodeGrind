4f4ad1d7e1d05a77062c97f935812234a6a7da3f
Nicholas Hebb <nhebb@breezetree.com>
2023-09-26 16:01:40 -0700

    Added LargestPalindromic

--- LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
+++ LeetcodeGrind/ArraysAndHashing/ArraysAndHashing.cs
@@ -3920,4 +3920,44 @@ public class ArraysAndHashing
 
         return board.Count;
     }
+
+
+    // 2384. Largest Palindromic Number
+    public string LargestPalindromic(string num)
+    {
+        var nums = new int[10];
+        foreach (var c in num)
+            nums[c - '0']++;
+
+        var maxSingle = -1;
+        var sb = new StringBuilder();
+
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            while (nums[i] > 1)
+            {
+                if (i > 0)
+                    sb.Append(i);
+                else
+                    break;
+
+                nums[i] -= 2;
+            }
+
+            if (nums[i] > 0)
+                maxSingle = Math.Max(maxSingle, i);
+        }
+
+        if (sb.Length == 0)
+            return maxSingle.ToString();
+
+        var sVal = sb.ToString();
+
+        if (maxSingle >= 0)
+            sVal = sVal + maxSingle.ToString() + string.Join("", sVal.Reverse());
+        else
+            sVal = sVal + string.Join("", sVal.Reverse());
+
+        return sVal;
+    }
 }
