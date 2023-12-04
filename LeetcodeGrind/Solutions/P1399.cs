9751aae02d89032e71753c31461a92c652836e5b
Nicholas Hebb <nhebb@breezetree.com>
2023-12-04 02:06:36 -0800

    Added solutions

--- LeetcodeGrind/Solutions/P1399.cs
+++ LeetcodeGrind/Solutions/P1399.cs
@@ -1,10 +1,30 @@
-using LeetcodeGrind.Common;
-
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 1399. Count Largest Group
 public class P1399
 {
+    public int CountLargestGroup(int n)
+    {
+        var d = new Dictionary<int, int>();
+        for (int i = 1; i <= n; i++)
+        {
+            var sum = 0;
+            var num = i;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
 
+            if (d.ContainsKey(sum))
+                d[sum]++;
+            else
+                d[sum] = 1;
+        }
+
+        var max = d.Values.Max();
+        var count = d.Values.Count(x => x == max);
+
+        return count;
+    }
 }
-
--- LeetcodeGrind/Solutions/P1544.cs
+++ LeetcodeGrind/Solutions/P1544.cs
@@ -1,10 +1,48 @@
-using LeetcodeGrind.Common;
+using System.Text;
 
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 1544. Make The String Great
 public class P1544
 {
+    public string MakeGood(string s)
+    {
+        if (s.Length == 1)
+            return s;
 
+        var sb = new StringBuilder();
+        var bad = true;
+
+        while (bad)
+        {
+            bad = false;
+            var i = 0;
+
+            while (i < s.Length - 1)
+            {
+                if (Math.Abs(s[i] - s[i + 1]) == 32)
+                {
+                    i += 2;
+                    bad = true;
+                }
+                else
+                {
+                    sb.Append(s[i]);
+                    i++;
+
+                }
+                if (i == s.Length - 1)
+                {
+                    sb.Append(s[i]);
+                }
+            }
+            s = sb.ToString();
+            sb.Clear();
+
+            if (s.Length == 1)
+                break;
+        }
+
+        return s;
+    }
 }
-
