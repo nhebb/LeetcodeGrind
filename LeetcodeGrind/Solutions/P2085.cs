0e757f5b46f64cf04ea973ccaa0883049896c278
Nicholas Hebb <nhebb@breezetree.com>
2023-12-18 22:17:42 -0800

    Added solutions

--- LeetcodeGrind/Solutions/P2085.cs
+++ LeetcodeGrind/Solutions/P2085.cs
@@ -1,10 +1,27 @@
-using LeetcodeGrind.Common;
-
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 2085. Count Common Words With One Occurrence
 public class P2085
 {
+    public int CountWords(string[] words1, string[] words2)
+    {
+        var d1 = words1.GroupBy(w => w)
+                       .ToDictionary(g => g.Key, g => g.Count());
 
+        var d2 = words2.GroupBy(w => w)
+                       .ToDictionary(g => g.Key, g => g.Count());
+
+        var count = 0;
+        foreach (var kvp in d1)
+        {
+            if (kvp.Value == 1 && 
+                d2.ContainsKey(kvp.Key) && 
+                d2[kvp.Key] == 1)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
-
--- LeetcodeGrind/Solutions/P2706.cs
+++ LeetcodeGrind/Solutions/P2706.cs
@@ -1,10 +1,29 @@
-using LeetcodeGrind.Common;
-
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 2706. Buy Two Chocolates
 public class P2706
 {
+    public int BuyChoco(int[] prices, int money)
+    {
+        var min1 = int.MaxValue;
+        var min2 = int.MaxValue;
 
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < min1)
+            {
+                min2 = min1;
+                min1 = prices[i];
+            }
+            else if (prices[i] < min2)
+            {
+                min2 = prices[i];
+            }
+        }
+
+        var cost = min1 + min2;
+        return cost <= money
+            ? money - cost
+            : money;
+    }
 }
-
