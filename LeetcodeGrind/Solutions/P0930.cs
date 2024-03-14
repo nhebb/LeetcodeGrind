63fde491287bbbe2dca9be561600780ff39dc267
Nicholas Hebb <nhebb@breezetree.com>
2024-03-14 01:56:47 -0700

    Added solutions

--- LeetcodeGrind/Solutions/P0930.cs
+++ LeetcodeGrind/Solutions/P0930.cs
@@ -1,10 +1,39 @@
-using LeetcodeGrind.Common;
-
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 930. Binary Subarrays With Sum
 public class P0930
 {
+    public int NumSubarraysWithSum(int[] nums, int goal)
+    {
+        // brute force - barey passes time limit
+        if (nums.Length == 1)
+        {
+            return nums[0] == goal
+                ? 1
+                : 0;
+        }
 
+        var count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var prev = 0;
+            for (int j = i; j < nums.Length; j++)
+            {
+                var curr = prev + nums[j];
+                if (curr > goal)
+                {
+                    break;
+                }
+
+                if (curr == goal)
+                {
+                    count++;
+                }
+                prev = curr;
+            }
+        }
+
+        return count;
+    }
 }
-
