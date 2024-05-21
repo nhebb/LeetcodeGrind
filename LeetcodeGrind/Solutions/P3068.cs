55783663f8b5b6b206ac9f6b1f763689ae600bfd
Nicholas Hebb <nhebb@breezetree.com>
2024-05-21 01:25:49 -0700

    Added solution

---
+++ LeetcodeGrind/Solutions/P3068.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetcodeGrind.Solutions;
+
+// 3068. Find the Maximum Sum of Node Values
+public class P3068
+{
+    public long MaximumValueSum(int[] nums, int k, int[][] edges)
+    {
+        var maxSum = 0L;
+        var diff = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            diff[i] = (nums[i] ^ k) - nums[i];
+            maxSum += nums[i];
+        }
+
+        Array.Sort(diff, (a, b) => b.CompareTo(a));
+
+        for (int i = 0; i < nums.Length - 1; i += 2)
+        {
+            long sum = diff[i] + diff[i + 1];
+
+            if (sum <= 0)
+            {
+                break;
+            }
+
+            maxSum += sum;
+        }
+
+        return maxSum;
+    }
+}
