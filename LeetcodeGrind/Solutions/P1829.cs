893a8b62cb2e8903d61ade8d670ae2192c4a7560
Nicholas Hebb <nhebb@breezetree.com>
2024-11-07 23:57:14 -0800

    Added solutions

--- LeetcodeGrind/Solutions/P1829.cs
+++ LeetcodeGrind/Solutions/P1829.cs
@@ -2,9 +2,27 @@ using LeetcodeGrind.Common;
 
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 1829. Maximum XOR for Each Query
 public class P1829
 {
+    public int[] GetMaximumXor(int[] nums, int maximumBit)
+    {
+        var prefix = new int[nums.Length];
+        var result = new int[nums.Length];
 
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            prefix[i] = prefix[i - 1] ^ nums[i];
+        }
+
+        var mask = (int)(Math.Pow(2, maximumBit) - 1);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = mask ^ prefix[^(i + 1)];
+        }
+
+        return result;
+    }
 }
-
--- LeetcodeGrind/Solutions/P2275.cs
+++ LeetcodeGrind/Solutions/P2275.cs
@@ -1,10 +1,28 @@
-using LeetcodeGrind.Common;
-
 namespace LeetcodeGrind.Solutions;
 
-// 
+// 2275. Largest Combination With Bitwise AND Greater Than Zero
 public class P2275
 {
+    public int LargestCombination(int[] candidates)
+    {
 
+        // max candidate is 10^7 => 24 bits
+        var bits = new int[24];
+
+        for (int i = 0; i < candidates.Length; i++)
+        {
+            var mask = 1;
+
+            for (int j = 0; j < 24; j++)
+            {
+                if ((candidates[i] & mask) > 0)
+                {
+                    bits[j]++;
+                }
+                mask *= 2;
+            }
+        }
+
+        return bits.Max();
+    }
 }
-
